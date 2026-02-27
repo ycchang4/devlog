e517b60eb7733aff35193f04be3ad08d9817be2c
ycchang4 <ycchang1117@gmail.com>
2026-02-26 16:17:49 -0800

    update: reroute to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,13 @@
 
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const { data: session } = useSession()
-
+  const router = useRouter()
   if (session) {
-    return (
-      <div className="container mt-5">
-        <h2>Signed in as {session.user?.name}</h2>
-        <img
-          src={session.user?.image ?? ''}
-          width={50}
-          className="rounded-circle my-3"
-        />
-
-        <div className="d-flex gap-2 mt-3">
-          <Link href="/new" className="btn btn-primary">
-            New Entry
-          </Link>
-          <button className="btn btn-danger" onClick={() => signOut()}>
-            Sign Out
-          </button>
-        </div>
-      </div>
-    )
+    router.push('/dashboard')
   }
 
   return (
@@ -33,7 +16,7 @@ export default function Home() {
       <h2>DevLog</h2>
       <button
         className="btn btn-dark mt-3"
-        onClick={() => signIn('github')}
+        onClick={() => signIn('github') }
       >
         Sign in with GitHub
       </button>
